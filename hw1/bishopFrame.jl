dc168e3d2021c68909ee960174436e0dd36246cb
Michal W. Tarnowski <michal.w.tarnowski@gmail.com>
2021-02-20 23:04:36 +0100

    Add hw1 starter code

---
+++ hw1/bishopFrame.jl
@@ -0,0 +1,109 @@
+using Makie
+using LinearAlgebra
+
+function topoint3f(xyz)
+    return Point3f0.(xyz[:,1], xyz[:,2], xyz[:,3])
+end
+
+# Sample Lissajous curve
+a = 3
+b = 2
+delta = pi / 2
+n = 100
+
+t = range(0, stop=2*pi, length=n)
+x = sin.(a * t * delta)
+y = sin.(b * t)
+z = .5 * t
+xyz = [x y z]
+
+# Edges and midpoints
+diff = xyz[2:end,:] - xyz[1:end-1,:]
+midpoints = (xyz[1:end-1,:] + xyz[2:end,:]) / 2
+
+## Problem 3.a
+binormal = zeros(n-2,3)
+### YOUR CODE TO COMPUTE BINORMAL HERE
+### END HOMEWORK PROBLEM ###
+
+scene1 = Scene()
+scene1 = lines!(x, y, z,
+                linewidth=4, color=:black)
+scene1 = arrows!(topoint3f(xyz[2:end-1,:]), topoint3f(binormal),
+                 linecolor=:red, arrowcolor=:red,
+                 linewidth=2, arrowsize=0.03)
+
+## Problem 3.b
+### WRITE ANY PRE-COMPUTATION CODE HERE ###
+### END PRE-COMPUTATION ###
+
+scene2 = Scene()
+scene2 = lines!(x, y, z,
+                linewidth=4, color=:black)
+scene2 = arrows!(topoint3f(midpoints),
+                 topoint3f(zeros(n-1, 3)),
+                 linecolor=:red, arrowcolor=:red,
+                 linewidth=2, arrowsize=0.03)
+scene2 = arrows!(topoint3f(midpoints),
+                 topoint3f(zeros(n-1, 3)),
+                 linecolor=:blue, arrowcolor=:blue,
+                 linewidth=2, arrowsize=0.03)
+uplot = scene2[end-1]
+vplot = scene2[end]
+
+scale = 0.25
+u = zeros(n - 1, 3)
+v = zeros(n - 1, 3)
+record(scene2, "bishop.gif", 0:5:360) do angle
+    t = diff[1,:] / norm(diff[1,:])
+    zaxis = [0;0;1]
+    frame0 = cross(t, zaxis)
+    frame1 = cross(frame0, t)
+
+    theta = angle * pi / 180
+    u[1,:] = cos(theta) .* frame0 + sin(theta) .* frame1
+    v[1,:] = -sin(theta) .* frame0 + cos(theta) .* frame1
+    for frame in 2:(n-1)
+        u[frame,:] .= 0
+        v[frame,:] .= 0
+        ### YOUR CODE TO FILL IN u AND v HERE ###
+        ### END HOMEWORK PROBLEM ###
+    end
+    uplot[2] = topoint3f(u.*scale)
+    vplot[2] = topoint3f(v.*scale)
+end
+
+
+## Problem 3.c
+# thetas is the variable you are supposed to update.
+thetas = range(0, stop=3*pi, length=n-1)
+m1 = cos.(thetas).*u + sin.(thetas).*v
+m2 = -sin.(thetas).*u + cos.(thetas).*v
+
+scene3 = Scene()
+scene3 = lines!(x, y, z,
+                linewidth=4, color=:black)
+scene3 = arrows!(topoint3f(midpoints),
+                 topoint3f(scale.*m1),
+                 linecolor=:red, arrowcolor=:red,
+                 linewidth=2, arrowsize=0.03)
+scene3 = arrows!(topoint3f(midpoints),
+                 topoint3f(scale.*m2),
+                 linecolor=:blue, arrowcolor=:blue,
+                 linewidth=2, arrowsize=0.03)
+m1plot = scene3[end-1]
+m2plot = scene3[end]
+
+nsteps = 200
+grad = zeros(n-1)
+record(scene3, "untwist.gif", 1:nsteps) do i
+    ### YOUR CODE HERE TO UPDATE thetas VIA GRADIENT DESCENT ###
+    ### END HOMEWORK PROBLEM ###
+    m1 = cos.(thetas).*u + sin.(thetas).*v
+    m2 = -sin.(thetas).*u + cos.(thetas).*v
+    m1plot[2] = topoint3f(scale.*m1)
+    m2plot[2] = topoint3f(scale.*m2)
+end
+
+# Uncomment to plot scene1
+# display(scene1)
---
+++ hw1/discreteCurve.jl
@@ -0,0 +1,73 @@
+using LinearAlgebra
+using Makie
+
+# Sample Lissajous curve
+a = 4
+b = 2
+delta = pi / 3
+n = 100
+
+t = range(0, stop=2*pi, length=n)
+x = sin.(a * t * delta)
+y = sin.(b * t)
+xy = [x y]
+
+## Problem 2.c
+n = length(x)
+u = zeros(n - 2)
+v = zeros(n - 2)
+### YOUR CODE HERE TO COMPUTE GRADIENT ###
+### END HOMEWORK PROBLEM ###
+
+# Plot curve and gradient vectors
+scene1 = Scene(show_axis=false, scale_plot=false)
+scene1 = lines!(scene1, x, y,
+                linewidth=3, color=RGBAf0(0.8, 0.05, 0.1))
+scene1 = arrows!(scene1, x[2:end-1], y[2:end-1], u, v,
+                 arrowsize=0.05)
+
+## Problem 2.d
+kappa = zeros(n-2)
+### YOUR CODE HERE TO COMPUTE KAPPA ###
+### END HOMEWORK PROBLEM ###
+c = to_colormap(kappa)
+
+scene2 = Scene(show_axis=false, scale_plot=false)
+scene2 = lines!(scene2, x, y, linewidth=4, color=c, colorrange=(0,4))
+scene2 = lines!(scene2,
+                zeros(length(c)) .+ (maximum(x) + 0.2),
+                range(minimum(y), stop=maximum(y), length=length(c)),
+                color=[minimum(c), maximum(c)], linewidth=30)
+
+## Problem 2.e
+function topoint2f(xy)
+    return Point2f0.(xy[:,1], xy[:,2])
+end
+
+t0 = 0
+t1 = pi * 1.25
+nsamples = 100
+# Modify nsteps if your method does not converge
+nsteps = 200
+
+# We provide a few examples of curves to try
+# curveFunction(t) = [cos(t)-cos(3*t).^3 sin(t)-sin(3*t).^3]
+# curveFunction(t) = [cos(t) sin(t)]
+curveFunction(t) = [t (t.-t0).*(t1.-t)]
+curve = vcat(curveFunction.(range(t0, stop=t1, length=nsamples))...)
+
+scene3 = Scene(show_axis=false, scale_plot=false)
+scene3 = lines!(scene3, curve[:,1], curve[:,2],
+                linewidth=3, color=RGBAf0(0.8, 0.05, 0.1))
+lineplt = scene3[end]
+record(scene3, "straighten.gif", 1:nsteps) do i
+    ### YOUR CODE HERE TO PERFORM GRADIENT DESCENT ###
+    ### END HOMEWORK PROBLEM ###
+    lineplt[1] = curve[:,1]
+    lineplt[2] = curve[:,2]
+end
+
+# Uncomment to display 2.c/2.d/2.e
+# display(scene1)
+# display(scene2)
+# display(scene3)
