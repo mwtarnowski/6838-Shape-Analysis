2c97e1588b62672c24721baccd19b4d1e913b0f8
Michal W. Tarnowski <michal.w.tarnowski@gmail.com>
2021-03-06 21:03:28 +0100

    Add code for meanCurve.jl

--- hw2/meanCurve.jl
+++ hw2/meanCurve.jl
@@ -10,9 +10,21 @@ X, T = readoff(filename)
 nv = size(X, 1)
 nt = size(T, 1)
 
+
+triangleArea(X1, X2, X3) = norm(cross(X1-X2, X3-X2)) / 2
+
 # ADD CODE TO COMPUTE SURFACE AREA HERE #############
 function surfaceArea(X, T)
-    return 0
+    area = 0
+    for i=1:nt
+        v1,v2,v3 = T[i,:]
+        X1 = X[v1,:]
+        X2 = X[v2,:]
+        X3 = X[v3,:]
+        A = triangleArea(X1, X2, X3)
+        area += A
+    end
+    return area
 end
 # END HOMEWORK ASSIGNMENT #########
 
@@ -20,7 +32,28 @@ end
 
 # ADD CODE TO COMPUTE COTANGENT LAPLACIAN HERE ##########
 function cotLaplacian(X, T)
-    return spzeros(nv, nv)
+    cots = zeros(nt, 3)
+    for i=1:nt
+        v1,v2,v3 = T[i,:]
+        e12 = X[v2,:] - X[v1,:]
+        e23 = X[v3,:] - X[v2,:]
+        e31 = X[v1,:] - X[v3,:]
+        cots[i,1] = -dot(e12, e31) / norm(cross(e12, e31))
+        cots[i,2] = -dot(e23, e12) / norm(cross(e23, e12))
+        cots[i,3] = -dot(e31, e23) / norm(cross(e31, e23))
+    end
+
+    I = [T[:,1]; T[:,2]; T[:,3]]
+    V = [cots[:,2]+cots[:,3]; cots[:,3]+cots[:,1]; cots[:,1]+cots[:,2]]
+    Ldiag = sparse(I, I, V, nv, nv)
+
+    I = [T[:,1]; T[:,2]; T[:,3]]
+    J = [T[:,2]; T[:,3]; T[:,1]]
+    V = [-cots[:,3]; -cots[:,1]; -cots[:,2]]
+    Ltri = sparse(I, J, V, nv, nv)
+
+    L = Ldiag + Ltri + Ltri'
+    return L
 end
 # END HOMEWORK ASSIGNMENT #######
 
@@ -32,7 +65,21 @@ println(norm(L - L'))
 
 # ADD CODE FOR DIVIDED DIFFERENCES HERE ######
 function dividedDifferences(X, T)
-    gradApprox = zeros(nv, 3);
+    gradApprox = zeros(nv, 3)
+    h = 0.001
+    hI = h * Matrix(I, 3, 3)
+    for i=1:nt
+        v1,v2,v3 = T[i,:]
+        X1 = X[v1,:]
+        X2 = X[v2,:]
+        X3 = X[v3,:]
+        for k=1:3
+            hv = hI[k,:]
+            gradApprox[v1,k] += (triangleArea(X1+hv, X2,    X3   ) - triangleArea(X1-hv, X2,    X3   )) / (2*h)
+            gradApprox[v2,k] += (triangleArea(X1,    X2+hv, X3   ) - triangleArea(X1,    X2-hv, X3   )) / (2*h)
+            gradApprox[v3,k] += (triangleArea(X1,    X2,    X3+hv) - triangleArea(X1,    X2,    X3-hv)) / (2*h)
+        end
+    end
     return gradApprox
 end
 # END HOMEWORK ASSIGNMENT #########
@@ -42,13 +89,28 @@ println(norm(.5 .* (L*X) - dividedDifferences(X,T)))
 
 # ADD CODE FOR COMPUTING THE BARYCENTRIC AREA VECTOR HERE ######
 function barycentricArea(X, T)
-    return zeros(nv)
+    M = zeros(nv)
+    for i=1:nt
+        v1,v2,v3 = T[i,:]
+        X1 = X[v1,:]
+        X2 = X[v2,:]
+        X3 = X[v3,:]
+        A = triangleArea(X1, X2, X3)
+        M[v1] += A / 3
+        M[v2] += A / 3
+        M[v3] += A / 3
+    end
+    return M
 end
 # END HOMEWORK ASSIGNMENT ########
 
 # ADD CODE FOR COMPUTING POINTWISE MEAN CURVATURE HERE #####
 function meanCurvature(X, T)
-    H = rand(nv)
+    L = cotLaplacian(X, T)
+    M = barycentricArea(X, T)
+    Hn = 0.5 * (L * X) ./ M
+    H = mapslices(norm, Hn, dims=[2])  # unsigned mean curvature
+    H = vec(H)
     return H
 end
 # END HOMEWORK ASSIGNMENT #######
@@ -62,8 +124,12 @@ function curvatureFlowEuler(X, T)
     Xt = copy(X)
     maxiters = 1000
     # ADD CODE FOR THE EXPLICIT INTEGRATOR HERE ####
+    tau = 5e-7 * surfaceArea(Xt, T)
+    L = cotLaplacian(Xt, T)
     for t=1:maxiters
-        Xt[:] = copy(Xt)
+        # L = cotLaplacian(Xt, T)
+        M = barycentricArea(Xt, T)
+        Xt[:] = Xt - tau * (L * Xt) ./ M
     end
     # END HOMEWORK ASSIGNMENT #####
     H = meanCurvature(Xt, T)
@@ -75,8 +141,12 @@ function curvatureFlowImplicit(X, T)
     Xt = copy(X)
     maxiters = 1000
     # ADD CODE FOR SEMI-IMPLICIT INTEGRATOR HERE ####
+    tau = 5e-7 * surfaceArea(Xt, T)
+    L = cotLaplacian(Xt, T)
     for t=1:maxiters
-        Xt[:] = copy(Xt)
+        # L = cotLaplacian(Xt, T)
+        M = barycentricArea(Xt, T)
+        Xt[:] = (spdiagm(0 => M) + tau * L) \ (M .* Xt)
     end
     # END HOMEWORK ASSIGNMENT
     H = meanCurvature(Xt, T)
